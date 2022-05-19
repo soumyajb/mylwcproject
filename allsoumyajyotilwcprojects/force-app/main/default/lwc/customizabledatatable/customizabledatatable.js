c3bf4bf32bebe42d36ffce2db0102edffe7865c5
Soumyajyoti Bhadra <soumyajyoti.bhadra@gmail.com>
2022-05-20 00:38:18 +0530

    Stabilized with Browser Persistence

--- allsoumyajyotilwcprojects/force-app/main/default/lwc/customizabledatatable/customizabledatatable.js
+++ allsoumyajyotilwcprojects/force-app/main/default/lwc/customizabledatatable/customizabledatatable.js
@@ -1,11 +1,12 @@
 import { LightningElement,track } from 'lwc';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent'
 import getField from '@salesforce/apex/AccountDataProvider.getField';
+import getAccount from '@salesforce/apex/AccountDataProvider.getAccount';
 
 export default class Customizabledatatable extends LightningElement {
 
-    headerObject=[];
-    bodyObject=[];
+    @track headerObject=[];
+    @track bodyObject=[];
     rowofrow=[];
     @track availableFields = [];
     defaultValues =
@@ -15,28 +16,117 @@ export default class Customizabledatatable extends LightningElement {
     {label:'Created Date',value: 'CreatedDate'}
     ];
     fieldsChosen = [];
+    fieldsChosenAPI = [];
+    currentConfig = JSON.parse(localStorage.getItem('previousConfig'));
+    //console.log(typeof currentConfig);
     
     showtable =false;
     showFieldChooserWindow =false;
+    showSpinner = false;
+    initdone=false;
     
-    
+    showUpdatedFields(event)
+    {
+        localStorage.setItem('previousConfig', JSON.stringify(this.fieldsChosen));
+        console.log(localStorage.getItem('previousConfig'));
+        this.bodyObject=[];
+        this.headerObject = [];
+        this.showtable =false;
+        this.showFieldChooserWindow =false;
+        this.showSpinner = true;
+        console.log(JSON.stringify(this.fieldsChosen));
+        getAccount({Fields:this.fieldsChosen}).then(data =>{
+            try {
+                console.table(data);
+                data.forEach( row=>
+                {
+                console.log(JSON.stringify(row));
+                
+                let currentTempObject = {};
+                Object.keys(row).forEach(fieldnamecurrent =>{
+                    let lowercaseKey = fieldnamecurrent.toLowerCase();
+                    currentTempObject[lowercaseKey] = row[fieldnamecurrent];
+                })
+                //console.table(row);
+                //console.table(currentTempObject);
+                //console.table(this.fieldsChosen);
+                row = JSON.parse(JSON.stringify(currentTempObject));
+                 let arrayofRowElements = [];
+                 if(!(this.fieldsChosen.includes('id') || this.fieldsChosen.includes('Id')))
+                 this.fieldsChosen.push('id');//added as Id will be absolutely queried as per design of apex
+
+                 this.fieldsChosen.forEach((key,index)=>{
+                    let found=false;
+                    Object.keys(row).forEach((fielname)=>{
+                        //console.log(`fielname.toLowerCase() : ${fielname.toLowerCase()} and key.toLowerCase: ${key.toLowerCase}`);
+                        if(fielname.toLowerCase() === key.toLowerCase())
+                        {
+                            found=true;
+                            let currentColumnValue = row[fielname];
+                            let currentkeyPair={key : fielname,value :  currentColumnValue};
+                            arrayofRowElements.push(currentkeyPair);
+                            
+                        }
+                    })
+                    if(found===false)
+                    {
+                        let currentkeyPair={key: key,value : ''};
+                        arrayofRowElements.push(currentkeyPair);
+                    }
+                    
+                    
+                })
+                this.bodyObject.push({id:row.id,values:arrayofRowElements});
+                
+                })
+                    
+
+               
+                this.fieldsChosen.forEach((tableheader,indexofelement)=>{
+                    this.headerObject.push({index:indexofelement,key :tableheader});
+                })
+                this.bodyObject = JSON.parse(JSON.stringify(this.bodyObject));
+                this.headerObject = JSON.parse(JSON.stringify(this.headerObject));
+                //console.clear();
+                //console.table(JSON.parse(JSON.stringify(this.bodyObject)));
+                //console.table(JSON.parse(JSON.stringify(this.headerObject)));
+                this.showtable =true;
+                this.showFieldChooserWindow =false;
+                this.showSpinner = false;
+
+            } catch (error) {
+                //console.log(error);
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'Error happened while retreiving the records',
+                    message: 'Error while retrieving the records.'+error,
+                    variant: 'error'
+                }));
+                this.showtable =false;
+                this.showFieldChooserWindow =false;
+                this.showSpinner = false;
+            }
+        })
+        
+    }
 
 
 
     openFieldChooser(event){
-        console.log('opening the Field Picker');
-               
+        //console.log('opening the Field Picker');
+        this.showtable =false;
+        this.showFieldChooserWindow =false;
+        this.showSpinner = true;       
         getField({ObjectName: 'Account'}).then(data=> {
         
-        console.log(data);
+        //console.log(data);
         data.forEach(element => {
             let currentOptionObject = {label:element.Label,value:element.Value};
             
             this.availableFields.push(currentOptionObject);
             
         });
-        console.log(JSON.stringify(this.availableFields));
-        console.table(this.availableFields);
+        //console.log(JSON.stringify(this.availableFields));
+        //console.table(this.availableFields);
         this.showFieldChooserWindow = true;
         this.showtable = false;
         }).catch(error =>{
@@ -48,20 +138,77 @@ export default class Customizabledatatable extends LightningElement {
             });
             
             this.dispatchEvent(errorEvent);
-            console.log(error);
+            //console.log(error);
         })
 
     }
     cancelModal(event)
     {
-        console.log('Cancelled the choice');
+        //console.log('Cancelled the choice');
         this.showFieldChooserWindow=false;
     }
     handlechange(event)
     {
-        console.table(JSON.parse(JSON.stringify(event.detail.value)));
+        //console.clear();
+        try {
+            let data = event.detail.value;
+            //console.log(JSON.stringify(data));   
+            //this.fieldsChosenAPI =         
+            this.fieldsChosen = [...data];
+            // data.forEach(element=>{
+            //     Array.from(JSON.parse(JSON.stringify(this.availableFields))).forEach(labelmap=>{
+            //         //console.log(`The value of element is ${element} and the label map ${labelmap.label} and the name is${labelmap.value}`)
+            //         if(element === labelmap.name)
+            //         {
+            //             this.fieldsChosenAPI.push(labelmap.value);
+            //             //console.log(`Added the ${labelmap.value} to fieldsChosenAPI`);
+            //         }
+            //     })
+                
+
+            // })
+            
+
+        }
+        catch(e)
+        {
+            //console.log(error);
+            let errorEvent = new ShowToastEvent({
+                title: 'Error occured',
+                message:
+                    'Error occured while updating requested field. Error: '+error,
+                variant:'error'
+            });
+            
+            this.dispatchEvent(errorEvent);
+            ;
+        }
+        
+
     }
     renderedCallback() {
+        try{
+            console.log(typeof this.currentConfig);
+            if(this.currentConfig.length !=null)
+            {
+                console.log('Existing config exists');
+                //this.currentConfig.split(',')
+                this.fieldsChosen  =  [...Array.from(this.currentConfig)];
+            }else
+            {
+                this.fieldsChosen = ['name','phone','email','website'];
+            }
+        if(!this.initdone)
+        {
+            this.initdone =true;
+            this.showUpdatedFields();
+        }
+        
+        }
+        catch(e)
+        {
+                console.log(e);
+        }
         
         
         }
